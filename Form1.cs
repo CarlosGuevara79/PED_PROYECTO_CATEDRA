af3aa281c220917fb802c78e5e9f99f288ccde82
Carlos Guevara <cdguevara2010@gmail.com>
2023-03-15 21:06:47 -0600

    first commit

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCatedra_PED_MR190768_GM172474_CM221954
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
